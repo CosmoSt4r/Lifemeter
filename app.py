43c246b687c2783f71a040a48ab773697cbc610c
Andrey Kalkutin <cosmostar@protonmail.com>
2021-01-01 19:13:48 +0500

    "Happy birthday" message added

--- app.py
+++ app.py
@@ -19,8 +19,12 @@ def life_info_page(date_of_birth):
         return abort(404)
 
     today = date.today()
-
     life_info = {'date_of_birth': date_of_birth}
+    if today.day == date_of_birth.day and today.month == date_of_birth.month:
+        life_info['today_is_birthday'] = True
+        print('yes')
+    else:
+        life_info['today_is_birthday'] = False
 
     lived = today - date_of_birth
     life_info['lived_days'] = lived.days
@@ -37,6 +41,8 @@ def life_info_page(date_of_birth):
     next_birthday_date = date(date_of_birth.year + ages + 1, date_of_birth.month, date_of_birth.day)
     to_next_birthday = next_birthday_date - today
     life_info['to_next_birthday'] = to_next_birthday.days
+
+    print(date.today() + timedelta(days=243))
 
     return render_template('life_info.html', life_info=life_info)
 
