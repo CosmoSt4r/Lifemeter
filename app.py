ff9008d47a4aaa138e3a5e5a85140e9cea44386f
Andrey Kalkutin <cosmostar@protonmail.com>
2021-01-02 14:50:27 +0500

    Passing parameters to charts added

--- app.py
+++ app.py
@@ -10,8 +10,15 @@ def index():
     return 'Hello World!'
 
 
-@app.route('/<date_of_birth>/')
-def life_info_page(date_of_birth):
+@app.route('/<date_of_birth>:<gender>')
+def life_info_page(date_of_birth, gender):
+
+    if gender == 'male':
+        life_expectancy = 78
+    elif gender == 'female':
+        life_expectancy = 82
+    else:
+        return abort(404)
 
     try:
         date_of_birth = date.fromisoformat(date_of_birth)
@@ -19,10 +26,11 @@ def life_info_page(date_of_birth):
         return abort(404)
 
     today = date.today()
-    life_info = {'date_of_birth': date_of_birth}
+    life_info = {'date_of_birth': date_of_birth,
+                 'life_expectancy':  life_expectancy}
+
     if today.day == date_of_birth.day and today.month == date_of_birth.month:
         life_info['today_is_birthday'] = True
-        print('yes')
     else:
         life_info['today_is_birthday'] = False
 
@@ -32,8 +40,10 @@ def life_info_page(date_of_birth):
     ages = (lived.days - number_of_leap_years(date_of_birth)) // 365
     life_info['ages'] = ages
 
-    lived_percentage = round(lived / timedelta(days=365*78) * 100, 2)
+    lived_percentage = round(lived / timedelta(days=365*life_expectancy) * 100, 2)
+    remain_life_percentage = round(100 - lived_percentage, 2)
     life_info['lived_percentage'] = lived_percentage
+    life_info['remain_life_percentage'] = remain_life_percentage
 
     left = timedelta(days=365*78) - lived
     life_info['left_days'] = left.days
@@ -41,8 +51,6 @@ def life_info_page(date_of_birth):
     next_birthday_date = date(date_of_birth.year + ages + 1, date_of_birth.month, date_of_birth.day)
     to_next_birthday = next_birthday_date - today
     life_info['to_next_birthday'] = to_next_birthday.days
-
-    print(date.today() + timedelta(days=243))
 
     return render_template('life_info.html', life_info=life_info)
 
